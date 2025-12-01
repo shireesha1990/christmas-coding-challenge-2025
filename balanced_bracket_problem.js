79c8ed133c961a99028143b07ac8b36a45277f4e
shireesha1990 <ps.shireesha@gmail.com>
2025-12-01 21:43:55 +0000

    balanced bracket problem added

---
+++ balanced_bracket_problem.js
@@ -0,0 +1,23 @@
+//Check given string is balanced bracket or not
+
+function isBalanced(str){
+  const stack = [];
+  const map= {')': '(', ']': '[','}': '{'};
+  for(let i =0; i<str.length; i++) {
+    if(str[i] === "(" || str[i] === "[" || str[i] === "{"){
+      stack.push(str[i]);
+    }
+    else if(stack[stack.length -1] === map[str[i]])
+   {
+     stack.pop()
+    }     
+    else return false;
+  }
+  return stack.length ? false : true;
+}
+
+export default function App() {
+  console.log("----")
+  console.log(isBalanced("{)))}"));
+//   return isBalanced("[()]");
+}
