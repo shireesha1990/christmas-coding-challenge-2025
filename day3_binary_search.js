a6be5dc3f6719e1f1dc426b25b2284ce6e7aaaa0
shireesha1990 <ps.shireesha@gmail.com>
2025-12-03 17:36:01 +0000

    implement binary search

---
+++ day3_binary_search.js
@@ -0,0 +1,21 @@
+// Binary search - Simple Binary search with array and target element as input 
+// Divide and Conquer
+// Eliminate half of the array in each iteration
+
+
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length -1;
+
+    while(left <= right) {
+        const mid = Math.floor(left+right)/2;
+        if(arr[mid] == target) return mid;
+        if(arr[mid] < target) left = mid + 1;
+    }
+    return -1;
+}
+
+let arr = [12,45,67,89,34,90];
+let target = 67;
+
+binarySearch(arr, target);
