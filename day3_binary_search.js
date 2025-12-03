2e41c5c0f209837f8360a184c8668e5944d9fd2f
shireesha1990 <ps.shireesha@gmail.com>
2025-12-03 17:38:17 +0000

    fix the logic

--- day3_binary_search.js
+++ day3_binary_search.js
@@ -11,6 +11,7 @@ function binarySearch(arr, target) {
         const mid = Math.floor(left+right)/2;
         if(arr[mid] == target) return mid;
         if(arr[mid] < target) left = mid + 1;
+        else if(arr[mid]> target) right = mid - 1;
     }
     return -1;
 }
